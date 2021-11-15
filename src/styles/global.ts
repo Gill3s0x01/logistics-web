08c3bfa70303f8685231adf85c9069981a39e124
Lorison-Gilles <l.gilles.pereira@avanade.com>
2021-11-15 13:23:59 -0300

    🎨 instaled styled components and create global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const GlobalStyle = styled.div`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Roboto', sans-serif;
+    font-size: 16px;
+    color: #333;
+    background-color: #fff;
+  }
+`
